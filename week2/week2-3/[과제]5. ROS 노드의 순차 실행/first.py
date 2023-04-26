226346af5d73128dd4cb9a205a59eeeac5afdc41
gks970113-woo <onerainj@gmail.com>
2023-04-27 02:52:43 +0900

    [과제]5.ROS 노드의 순차 실행 업로드

---
+++ week2/week2-3/[과제]5. ROS 노드의 순차 실행/first.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+name = "first"
+pub_topic = "msg_to_receiver"
+sub_topic = "start_ctl"
+
+OK = None
+
+def ctl_callback(data):
+	global OK
+	OK = str(data.data)
+
+rospy.init_node(name)
+rospy.Subscriber(sub_topic, String, ctl_callback)
